04dcc2a2072c23bf00c1df87ec1e82ea6b34e12a
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-11-11 19:30:26 -0800

    Add more files and routers

---
+++ public/app/app.js
@@ -0,0 +1,57 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('pulse', ['ngCookies', 'pulse.user', 'pulse.playlist', 'pulse.auth', 'pulse.factory', 'pulse.main'])
+.constant('ApiEndpoint', {
+  url: 'http://localhost:8100/api'
+})
+.config(function ($stateProvider, $httpProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('main');
+
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.withCredentials = true;
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  $httpProvider.defaults.headers.common["Accept"] = "application/json";
+  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+
+  $stateProvider
+    .state('main', {
+      url: '/main',
+      templateUrl: '/app/main/main.html',
+      controller: 'MainController'
+    })
+    .state('playlist', {
+      url: '/playlist',
+      templateUrl: '/app/playlist/playlist.html',
+      controller: 'PlaylistController'
+    })
+    .state('user', {
+      url: '/user',
+      templateUrl: '/app/user/user.html',
+      controller: 'UserController'
+    });
+
+  $httpProvider.interceptors.push('AttachTokens');
+})
+// .factory('AttachTokens', function ($window) {
+//   var attach = {
+//     request: function (object) {
+//       var jwt = $window.localStorage.getItem('com.shortly');
+//       if (jwt) {
+//         object.headers['x-access-token'] = jwt;
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return attach;
+// })
+// .run(function ($rootScope, $location, Auth) {
+//   $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+//       $location.path('/signin');
+//     }
+//   });
+// });
