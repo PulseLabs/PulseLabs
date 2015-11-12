a8f3ac8672ada281296c0a4fec7363089d6fc66e
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-11-11 20:01:44 -0800

    Add css styling for the main page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(function(req, res) {
-  res.status(200).send("Hello World");
-});
+app.use(express.static('public'));
 
 app.listen(8888, function(req, res) {
-  console.log('Server is running.');
+  console.log('Server is running.' + 8888);
 });
