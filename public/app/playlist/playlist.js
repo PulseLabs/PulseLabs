382ee67cd0ee26d04599b65d84c6d7bcc8f837c5
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2015-11-12 19:22:54 -0800

    fixed merge conflict in index.css

--- public/app/playlist/playlist.js
+++ public/app/playlist/playlist.js
@@ -1,4 +1,16 @@
 angular.module('pulse.playlist', [])
-.controller('PlaylistController', function ($scope) {
-  
+.controller('PlaylistController', function ($scope, $http) {
+  $scope.tracks = [];
+  $scope.searchTrack = function () {
+    var searchStr = $scope.search.split(" ").join("+");
+    console.log(searchStr);
+    $http({
+      method: 'GET',
+      withCredentials: false,
+      url: 'http://api.spotify.com/v1/search?q='+searchStr+'&type=track'
+    }).then(function (result) {
+      console.log(result.data.tracks.items);
+      $scope.tracks = result.data.tracks.items;
+    })
+  };
 });
