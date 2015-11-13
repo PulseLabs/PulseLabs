dcdf413f994d7bef95fec642abb17df3c6e3829d
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-11-13 11:54:20 -0800

    Modify PlaylistModel

--- server/user/userModel.js
+++ server/user/userModel.js
@@ -1,20 +1,11 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
-var Q = require('q');
-var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
     unique: true
-  },
-
-  userId: {
-    type: String,
-    required: true
-  },
-  salt: String
+  }
 });
 
 module.exports = mongoose.model('users', UserSchema);
