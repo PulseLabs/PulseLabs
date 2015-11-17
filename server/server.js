18e9af551b16abe670c6261b81f5b5f6080395bb
Ying Hao Chan <yinghao@outlook.com>
2015-11-16 17:22:19 -0800

    changed to process.env for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var db = 'mongodb://pulse:hr352015@ds053954.mongolab.com:53954/pulse-dev';
+var db = process.env.DB || 'mongodb://localhost/pulse';
 var app = express();
 var Q = require('q');
 var port = process.env.PORT || 8888;
@@ -8,7 +8,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //connect to mongo db here
-mongoose.connect('mongodb://localhost/pulse');
+mongoose.connect(db);
 app.use(express.static('public'));
 
 require('./config/middleware.js')(app, io, express);
