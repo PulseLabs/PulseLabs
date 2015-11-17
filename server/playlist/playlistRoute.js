5941d031e7653b2c9db8407bd32b77b0cf425f0a
SunyoungKim508 <sunyoungkim508@gmail.com>
2015-11-16 18:10:37 -0800

    Add song is working

--- server/playlist/playlistRoute.js
+++ server/playlist/playlistRoute.js
@@ -4,13 +4,13 @@ module.exports = function (app) {
   // app === userRouter injected from middlware.js
 
   app.post('/create', playlistController.newPlaylist);
-  app.param('code', function (req, res, next) {
-    console.log(req.params.code);
-    if (!req.params.code) {
-      req.params.code = '';
-    }
-    next();
-  });
+  // app.param('code', function (req, res, next) {
+  //   console.log(req.params.code);
+  //   if (!req.params.code) {
+  //     req.params.code = '';
+  //   }
+  //   next();
+  // });
   app.post('/:code/add', playlistController.addSong);
   app.post('/:code', playlistController.gotoPlaylist);
   app.get('/:code', playlistController.gotoPlaylist);
--- server/song/songModel.js
+++ server/song/songModel.js
@@ -3,25 +3,28 @@ var Q = require('q');
 
 var Schema = mongoose.Schema;
 var SongSchema = new Schema({
-  songname: {
-    type: String,
-    required: true
-  },
+  // songname: {
+  //   type: String
+  //   // ,
+  //   // required: true
+  // },
 
-  artist: {
-    type: String,
-    required: true
-  },
+  // artist: {
+  //   type: String
+  //   // ,
+  //   // required: true
+  // },
 
   uri: {
-    type: String,
-    required: true
+    type: String
+    // ,
+    // required: true
   },
 
-  image: {
-    type: String,
-    default: 'https://d3m79pznqer0b2.cloudfront.net/default-album-image.jpg'
-  },
+  // image: {
+  //   type: String,
+  //   default: 'https://d3m79pznqer0b2.cloudfront.net/default-album-image.jpg'
+  // },
 
   order: {
     type: Number
@@ -30,3 +33,10 @@ var SongSchema = new Schema({
 
 module.exports = SongSchema;
 // module.exports = mongoose.model('songs', SongSchema);
+
+
+// songname: name
+// artist:   album.artists[0].name
+// uri:      uri
+// image:    album.images[0].url
+// order:
